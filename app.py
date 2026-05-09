61e2db8715a782a67f7db9c724b7b847ba23bda3
Janani <djananipriya09@gmail.com>
2026-05-09 16:59:39 +0530

    trigger pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello! My first API is live."
+    return "Hello! My first API is live. Version 2."
 
 @app.route("/health")
 def health():
